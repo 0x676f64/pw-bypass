9d988cf98329bf44f6bbccdac33ef7d945f73c8c
Joey Rodriguez <oscardiggz@protonmail.com>
2024-11-05 12:00:52 -0600

    Create program.bat

---
+++ program.bat
@@ -0,0 +1,8 @@
+// Trying to download a program on Winodws without knowing the users password 
+// Whatever the program is called that you are trying to download, make a copy and make the extension .txt
+// Change the extension to .bat
+// type these two lines of code 
+
+Set __COMPAT_LAYER=RunAsInvoker
+Start program.exe 
+
